75ed940c363151669fd8e121d35be1790681d3c5
asmaylbwzan81 <asmaylbwzan81@gmail.com>
2026-05-06 00:21:16 +0300

    Update voting_engine.py

--- voting_engine.py
+++ voting_engine.py
@@ -1,6 +1,7 @@
 def vote(scores):
-    total = sum(scores.values())
-    return total / len(scores)
+    values = list(scores.values())
+    total = sum(values)
+    return total / len(values)
 
 def decision(score):
     if score >= 0.70:
